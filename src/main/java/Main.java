3d2536c81cdc293297b7324c2b7415c94b27c61a
Guy Gabel <guy.gabel0@gmail.com>
2023-01-11 23:15:35 +0200

    all methods in classes main got just one method while loop for start over with case-sensitive commend

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String startOver;
-        do {
-
-            WelcomeCoinAmount start = new WelcomeCoinAmount();
-            start.welcomecoinamount();
-
-            Scanner scanner2 = new Scanner(System.in);
-            System.out.println("That's it!");
-            System.out.println("Do you want to start over?  \n Y - yes, let's start over \n N - no that's it");
-            startOver = scanner2.next();
-        }
-        while (startOver.equals("Y"));
+        TryAgainLoop converterWithLoop = new TryAgainLoop();
+        converterWithLoop.tryAgain();
+        
 
 
     }
