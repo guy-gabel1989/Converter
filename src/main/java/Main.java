d063ee06a197e904b7578c879ce9268924ae4fdb
Guy Gabel <guy.gabel0@gmail.com>
2023-01-11 22:49:38 +0200

    Start over loop created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String startOver;
+        do {
 
-        WelcomeCoinAmount start = new WelcomeCoinAmount();
-        start.welcomecoinamount();
+            WelcomeCoinAmount start = new WelcomeCoinAmount();
+            start.welcomecoinamount();
 
-
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("That's it!");
+            System.out.println("Do you want to start over?  \n Y - yes, let's start over \n N - no that's it");
+            startOver = scanner2.next();
+        }
+        while (startOver.equals("Y"));
 
 
     }
