47f8d7fd50e178a725e4453bb0d443b3c007d8ff
Guy Gabel <guy.gabel0@gmail.com>
2023-01-16 22:50:19 +0200

    Final version - tested and approved

--- src/main/java/ResultPrint.java
+++ src/main/java/ResultPrint.java
@@ -5,7 +5,9 @@ import java.nio.file.Paths;
 public class ResultPrint {
     void resultPrint() throws IOException {
 
+        //file path for the file the holds all the calculations
         String filePath = "Result List";
+        //prints evrything that's in the Result List file
         String resultsPrint = new String(Files.readAllBytes(Paths.get(filePath)));
         System.out.println(resultsPrint);
     }
--- src/main/java/TryAgainLoop.java
+++ src/main/java/TryAgainLoop.java
@@ -2,17 +2,22 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TryAgainLoop {
+    //wrapping the calculation in a loop to make sure the user stay in the program
     void tryAgain(){
+        //initializing startOver object
         String startOver = null;
         do {
+            //calling the CalculateAndSaveResult class to initiate the coin creation
             CalculateAndSaveResult start = new CalculateAndSaveResult();
             start.calculateAndSaveResult();
 
             Scanner scanner2 = new Scanner(System.in);
             System.out.println("That's it!");
+            //ask the user if he is done; Y will start over, N end the method and the program will continue in the main class
             System.out.println("Do you want to start over?  \n Y - yes, let's start over \n N - no that's it");
             do{
                 try {
+                    //try-catch block to make sure the program doesn't crash or exits early
                     startOver = scanner2.next();
                     if (!startOver.equalsIgnoreCase("Y") && !startOver.equalsIgnoreCase("N")) {
                         System.out.println("Invalid input, Please type Y or N");
