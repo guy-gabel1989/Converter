22e4391d0de1643c98d89999a5cda1f87c2f81b6
Guy Gabel <guy.gabel0@gmail.com>
2023-01-16 20:35:06 +0200

    First running version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,16 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //First Screen - Welcome message
         WelcomeMessage firstWelcome = new WelcomeMessage();
         firstWelcome.welcomeMessage();
+
         TryAgainLoop converterWithLoop = new TryAgainLoop();
         converterWithLoop.tryAgain();
 
-        String filePath = "Result List";
-        String resultsPrint = new String(Files.readAllBytes(Paths.get(filePath)));
-        System.out.println(resultsPrint);
-        
-
-
+        ResultPrint resultPrint = new ResultPrint();
+        resultPrint.resultPrint();
     }
 
 }
--- src/main/java/TryAgainLoop.java
+++ src/main/java/TryAgainLoop.java
@@ -6,15 +6,16 @@ public class TryAgainLoop {
         do {
 
             WelcomeCoinAmount start = new WelcomeCoinAmount();
-
             start.welcomecoinamount();
-
-
 
             Scanner scanner2 = new Scanner(System.in);
             System.out.println("That's it!");
             System.out.println("Do you want to start over?  \n Y - yes, let's start over \n N - no that's it");
             startOver = scanner2.next();
+            if (!startOver.equalsIgnoreCase("Y") && !startOver.equalsIgnoreCase("N")) {
+                System.out.println("Invalid input, Please type Y or N");
+                startOver = scanner2.next();
+            }
         }
         while (startOver.equalsIgnoreCase("Y"));
 
